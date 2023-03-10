39af651f2267e3309c1ffba07f3195135f0e5edf
pi2 <fyp.gch6@gmail.com>
2023-03-10 16:43:56 +0800

    temporary topic change

--- packet_forwarder/src/lora_pkt_fwd.c
+++ packet_forwarder/src/lora_pkt_fwd.c
@@ -1900,9 +1900,9 @@ int main(int argc, char ** argv)
         pthread_mutex_lock(&mx_stat_rep);
         int rc;
         if (((gps_enabled == true) && (coord_ok == true)) || (gps_fake_enable == true)) {
-            rc = snprintf(status_report, STATUS_SIZE, "\"stat\":{\"eui\":\"0x%016" PRIx64 "\",\"time\":\"%s\",\"location\":{\"lati\":%.5f,\"long\":%.5f,\"alti\":%i},\"metrics\":{\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}}}", eui, stat_timestamp, cp_gps_coord.lat, cp_gps_coord.lon, cp_gps_coord.alt, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
+            rc = snprintf(status_report, STATUS_SIZE, "{\"stat\":{\"eui\":\"0x%016" PRIx64 "\",\"time\":\"%s\",\"location\":{\"lati\":%.5f,\"long\":%.5f,\"alti\":%i},\"metrics\":{\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}}}", eui, stat_timestamp, cp_gps_coord.lat, cp_gps_coord.lon, cp_gps_coord.alt, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
         } else {
-            rc = snprintf(status_report, STATUS_SIZE, "\"stat\":{\"eui\":\"0x%016" PRIx64 "\",\"time\":\"%s\",\"metrics\":{\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}}}", eui, stat_timestamp, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
+            rc = snprintf(status_report, STATUS_SIZE, "{\"stat\":{\"eui\":\"0x%016" PRIx64 "\",\"time\":\"%s\",\"metrics\":{\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}}}", eui, stat_timestamp, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
         }
         if (rc < 0) { printf("Error on snprintf overflow\n"); exit(-1); }
 
@@ -1910,7 +1910,7 @@ int main(int argc, char ** argv)
         pthread_mutex_unlock(&mx_stat_rep);
         
         if (mqtt_connected){ 
-            publish_mqtt(mqtt_client, mqttTopic, status_report);
+            publish_mqtt(mqtt_client, TOPIC_STATUS, status_report);
         }
     }
 

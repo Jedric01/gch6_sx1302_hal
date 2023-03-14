f00ea4ac3927bb7b15833ab296e4bd78d71b163a
pi-868M <oscarwdh7@gmail.com>
2023-03-14 16:30:31 +0800

    fix JSON format error
    leave status_report for TTN untouched
    thread_up prefixes buffer with '{'

--- packet_forwarder/src/lora_pkt_fwd.c
+++ packet_forwarder/src/lora_pkt_fwd.c
@@ -1902,10 +1902,10 @@ int main(int argc, char ** argv)
         pthread_mutex_lock(&mx_stat_rep);
         int rcs, rcm;
         if (((gps_enabled == true) && (coord_ok == true)) || (gps_fake_enable == true)) {
-            rcs = snprintf(status_report, STATUS_SIZE, "{\"stat\":{\"eui\":\"0x%016" PRIx64 "\",\"time\":\"%s\",\"location\":{\"lati\":%.5f,\"long\":%.5f,\"alti\":%i},\"metrics\":{\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}}}", eui, stat_timestamp, cp_gps_coord.lat, cp_gps_coord.lon, cp_gps_coord.alt, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
+            rcs = snprintf(status_report, STATUS_SIZE, "\"stat\":{\"time\":\"%s\",\"lati\":%.5f,\"long\":%.5f,\"alti\":%i,\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}", stat_timestamp, cp_gps_coord.lat, cp_gps_coord.lon, cp_gps_coord.alt, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
             rcm = snprintf(mqtt_gateway_status, STATUS_SIZE, "{\"stat\":{\"eui\":\"0x%016" PRIx64 "\",\"time\":\"%s\",\"location\":{\"lati\":%.5f,\"long\":%.5f,\"alti\":%i},\"metrics\":{\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}}}", eui, stat_timestamp, cp_gps_coord.lat, cp_gps_coord.lon, cp_gps_coord.alt, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
         } else {
-            rcs = snprintf(status_report, STATUS_SIZE, "{\"stat\":{\"eui\":\"0x%016" PRIx64 "\",\"time\":\"%s\",\"metrics\":{\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}}}", eui, stat_timestamp, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
+            rcs = snprintf(status_report, STATUS_SIZE, "\"stat\":{\"time\":\"%s\",\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}", stat_timestamp, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
             rcm = snprintf(mqtt_gateway_status, STATUS_SIZE, "{\"stat\":{\"eui\":\"0x%016" PRIx64 "\",\"time\":\"%s\",\"metrics\":{\"rxnb\":%u,\"rxok\":%u,\"rxfw\":%u,\"ackr\":%.1f,\"dwnb\":%u,\"txnb\":%u,\"temp\":%.1f}}}", eui, stat_timestamp, cp_nb_rx_rcv, cp_nb_rx_ok, cp_up_pkt_fwd, 100.0 * up_ack_ratio, cp_dw_dgram_rcv, cp_nb_tx_ok, temperature);
         }
         if (rcs < 0) { printf("[GCH6] Error on snprintf overflow\n"); }
@@ -2471,6 +2471,7 @@ void *thread_up(void* args) {
             }
         }
 
+        // [GCH6] this thread already prefix buffer (status_report) with '{'
         /* add status report if a new one is available */
         if (send_report == true) {
             pthread_mutex_lock(&mx_stat_rep);
